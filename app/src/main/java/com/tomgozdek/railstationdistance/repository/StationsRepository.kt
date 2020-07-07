9fd6f5732604556166fe21ac9799f72517837caf
tomgozdek <tomgozdek@gmail.com>
2020-07-07 23:58:30 +0200

    Add stations repository

---
+++ app/src/main/java/com/tomgozdek/railstationdistance/repository/StationsRepository.kt
@@ -0,0 +1,9 @@
+package com.tomgozdek.railstationdistance.repository
+
+import com.tomgozdek.railstationdistance.database.StationDatabase
+
+class StationsRepository(private val database: StationDatabase){
+    fun reloadData(){
+
+    }
+}
