8ed9a302a6fa94993c2a2a90e10c19f607a4f750
tomgozdek <tomgozdek@gmail.com>
2020-07-07 19:40:20 +0200

    Setup navigation component and data binding for DistanceFragment

--- app/src/main/java/com/tomgozdek/railstationdistance/distance/DistanceFragment.kt
+++ app/src/main/java/com/tomgozdek/railstationdistance/distance/DistanceFragment.kt
@@ -1,7 +1,26 @@
 package com.tomgozdek.railstationdistance.distance
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.tomgozdek.railstationdistance.R
+import com.tomgozdek.railstationdistance.databinding.FragmentDistanceBinding
 
 class DistanceFragment : Fragment(){
 
+    private lateinit var binding : FragmentDistanceBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_distance, container, false)
+
+        return binding.root
+    }
 }
