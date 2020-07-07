cf7912f6b4f152351e44c9fd2591103a56b2bcc0
tomgozdek <tomgozdek@gmail.com>
2020-07-07 22:12:06 +0200

    Move base api url to service class

--- app/src/main/java/com/tomgozdek/railstationdistance/network/KoleoApi.kt
+++ app/src/main/java/com/tomgozdek/railstationdistance/network/KoleoApi.kt
@@ -6,12 +6,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object KoleoApi {
-    private const val BASE_URL = "https://koleo.pl/api/v2/main/"
 
     val service : KoleoService by lazy {
         Retrofit.Builder()
             .addConverterFactory(MoshiConverterFactory.create(getMoshi()))
-            .baseUrl(BASE_URL)
+            .baseUrl(KoleoService.BASE_URL)
             .build()
             .create(KoleoService::class.java)
     }
--- app/src/main/java/com/tomgozdek/railstationdistance/network/KoleoService.kt
+++ app/src/main/java/com/tomgozdek/railstationdistance/network/KoleoService.kt
@@ -5,12 +5,15 @@ import retrofit2.http.GET
 import retrofit2.http.Headers
 
 interface KoleoService {
+    companion object  {
+        const val BASE_URL : String = "https://koleo.pl/api/v2/main/"
+    }
 
-    @Headers("X-KOLEO-Version : 1")
+    @Headers("X-KOLEO-Version: 1")
     @GET("stations")
     fun getStations() : Call<List<Station>>
 
-    @Headers("X-KOLEO-Version : 1")
+    @Headers("X-KOLEO-Version: 1")
     @GET("station_keywords")
     fun getStationKeywords() : Call<List<StationKeyword>>
 }
