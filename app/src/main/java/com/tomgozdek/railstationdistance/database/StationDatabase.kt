5160690462ece491bb6b3628c4518276752d651d
tomgozdek <tomgozdek@gmail.com>
2020-07-07 23:58:09 +0200

    Rename StationDatabase

--- app/src/main/java/com/tomgozdek/railstationdistance/database/StationDatabase.kt
+++ app/src/main/java/com/tomgozdek/railstationdistance/database/StationDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(entities = [Station::class, StationKeyword::class], version = 1)
-abstract class Database : RoomDatabase() {
+abstract class StationDatabase : RoomDatabase() {
     abstract fun stationDao() : StationDao
 
     abstract fun stationKeywordDao() : StationKeywordDao
