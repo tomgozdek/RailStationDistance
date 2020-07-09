12d3132ff3934d938a1ee782e15de4decffa4ffc
tomgozdek <tomgozdek@gmail.com>
2020-07-09 21:02:00 +0200

    Add onConflictStrategy to DAO

--- app/src/main/java/com/tomgozdek/railstationdistance/database/StationDao.kt
+++ app/src/main/java/com/tomgozdek/railstationdistance/database/StationDao.kt
@@ -2,11 +2,12 @@ package com.tomgozdek.railstationdistance.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
 interface StationDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(stations : List<Station>)
 
     @Query("SELECT * FROM stations_table WHERE id LIKE :stationId")
--- app/src/main/java/com/tomgozdek/railstationdistance/database/StationKeywordDao.kt
+++ app/src/main/java/com/tomgozdek/railstationdistance/database/StationKeywordDao.kt
@@ -2,11 +2,12 @@ package com.tomgozdek.railstationdistance.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
 interface StationKeywordDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(keywords: List<StationKeyword>)
 
     @Query("SELECT * FROM station_keywords_table")
